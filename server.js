457cc6e702feb0a8831a0f11db7c95aadf0f8441
ayfor <josh.a.stubbington@gmail.com>
2021-05-24 23:09:59 -0400

    npm start and server

---
+++ server.js
@@ -0,0 +1,20 @@
+//DEPENDENCIES
+
+const express = require('express');
+
+const app = express();
+
+
+const PORT  = process.env.PORT || 8081;
+
+
+app.use(express.urlencoded({ extended:true }));
+app.use(express.json());
+
+
+require('./routes/apiRoutes.js')(app);
+require('./routes/htmlRoutes.js')(app);
+
+app.listen(PORT, ()=>{
+    console.log(`NOTE TAKER started, Listening on PORT ${PORT}`);
+});
